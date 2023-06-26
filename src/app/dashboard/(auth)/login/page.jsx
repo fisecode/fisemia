bdf8f6f6fcd5f3b0b9cf73e4f4b6fa677e2680bc
fisecode <akhmatfikriseptiawan@gmail.com>
2023-06-26 17:46:36 +0700

    Update UI Google Button

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,51 +1,81 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import { signIn, useSession } from "next-auth/react";
+import { getProviders, signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Login = () => {
   const session = useSession();
   const router = useRouter();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setError(params.get("error"));
+    setSuccess(params.get("success"));
+  }, []);
 
   useEffect(() => {
     if (session.status === "authenticated") {
-      router.replace("/dashboard");
+      router.push("/dashboard");
     }
   }, [session.status, router]);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
     const email = e.target[0].value;
     const password = e.target[1].value;
 
-    signIn("credentials", { email, password });
-  };
-
-  const handleGoogleSignIn = () => {
-    signIn("google");
+    signIn("credentials", {
+      email,
+      password,
+    });
   };
 
   return (
     <div className={styles.container}>
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <h1 className={styles.title}>{success ? success : "Welcome Back"}</h1>
+      <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
+
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
-          type="email"
-          placeholder="email"
-          className={styles.input}
+          type="text"
+          placeholder="Email"
           required
+          className={styles.input}
         />
         <input
           type="password"
-          placeholder="password"
-          className={styles.input}
+          placeholder="Password"
           required
+          className={styles.input}
         />
         <button className={styles.button}>Login</button>
+        {error && <p className={styles.error}>{error}</p>}
       </form>
-      <button onClick={handleGoogleSignIn}>Login with Google</button>
+      <button
+        onClick={() => {
+          signIn("google");
+        }}
+        className={`${styles.button} ${styles.google}`}
+      >
+        Login with Google
+      </button>
+      <span className={styles.or}>- OR -</span>
+      <Link href="/dashboard/register" passHref className={styles.link}>
+        Create new account
+      </Link>
+      {/* <button
+        onClick={() => {
+          signIn("github");
+        }}
+        className={`${styles.button} ${styles.github}`}
+      >
+        Login with Github
+      </button> */}
     </div>
   );
 };
